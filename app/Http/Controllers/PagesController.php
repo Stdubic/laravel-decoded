cafd86527c3d0473794e58aea127180082a9b55c
Stdubic <stevandaleksic@gmail.com>
2016-02-24 12:36:40 +0100

    Adding auth.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,22 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class PagesController extends Controller
-{
-    public function home(){
+class PagesController extends Controller {
+	public function home() {
 
 
-	    $people = ['Steva','Pera', 'Mika'];
-	    return view('welcome', compact('people'));
+		$people = [ 'Steva', 'Pera', 'Mika' ];
 
-    }
+		return view( 'welcome', compact( 'people' ) );
 
-	public function about(){
+	}
 
-		return view('pages.about');
+	public function about() {
+
+		return view( 'pages.about' );
 	}
 }
